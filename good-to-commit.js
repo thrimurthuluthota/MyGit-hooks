6d9a74c8df4132eaef576aa701dc16b36b8c5b83
unknown <1500109@INNR90A3XRR.zone1.scb.net>
2015-04-27 12:56:14 +0530

    lets do this

--- good-to-commit.js
+++ good-to-commit.js
@@ -1,22 +1,7 @@
-require('shelljs/global');
+var Car = require('model/Car');
+var version = Car.exec('node --version', {silent:true}).output;
+console.log('version ' + version);
 
-var outputStr = exec('git diff --cached --name-only --diff-filter=ACM', {silent:true}).output.trim();
-var modifiedFiles = outputStr.split(/\n/);
 
-var importantFile = ['.js$'];
-
-var found = importantFile.some(function(s) {
-  return modifiedFiles.indexOf(s) != -1;
-});
-
-if(found) {
-  echo("  Error: Please do not commit these files")
-  #echo("  1) package.json");
-  #echo("  2) bower.json");
-  #echo("  ");
-  #echo("  Use the following command to fix this");
-  #echo("  git reset HEAD package.json bower.json");
-  process.exit(1);
-}
 
 
